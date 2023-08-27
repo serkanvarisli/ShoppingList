be6313541809f0bd488a7c63c164805be212c000
Serkan Varışlı <87394796+serkanvarisli@users.noreply.github.com>
2023-08-28 00:37:27 +0300

    file update admin

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -9,14 +9,13 @@ using Microsoft.AspNetCore.Http;
 
 namespace ShoppingList.Controllers
 {
-    [Authorize(AuthenticationSchemes="AdminAuthentication")]
+    [Authorize(AuthenticationSchemes = "AdminAuthentication")]
     public class AdminController : Controller
     {
-        //[Authorize]
         MyDbContext _context;
         public object PageUtility { get; private set; }
 
-		public AdminController(MyDbContext context)
+        public AdminController(MyDbContext context)
         {
             _context = context;
         }
@@ -35,13 +34,11 @@ namespace ShoppingList.Controllers
                     ProductId = p.ProductId
                 })
                 .ToList();
-
             return View(product);
         }
         [HttpGet]
-        
         public IActionResult AddProduct()
-		{
+        {
             var categories = _context.Categories.Select(c => new SelectListItem
             {
                 Value = c.CategoryId.ToString(),
@@ -50,9 +47,9 @@ namespace ShoppingList.Controllers
 
             ViewBag.Categories = categories;
             return View();
-		}
+        }
         [HttpPost]
-        public async Task <IActionResult> AddProduct(AdminAddFileViewModel adminAddFileViewModel,IFormFile fileUpload)
+        public async Task<IActionResult> AddProduct(AdminAddFileViewModel adminAddFileViewModel, IFormFile fileUpload)
         {
             if (_context.Products.Any(c => c.ProductName == adminAddFileViewModel.ProductName))
             {
@@ -62,11 +59,9 @@ namespace ShoppingList.Controllers
             var product = new Product
             {
                 ProductName = adminAddFileViewModel.ProductName,
-                //ProductImage = adminAddFileViewModel.ProductImage,
                 CategoryId = adminAddFileViewModel.CategoryId
             };
-
-            if (fileUpload!=null)
+            if (fileUpload != null)
             {
                 var localPath = "/wwwroot/images/products/";
                 var path = Path.Join(Directory.GetCurrentDirectory(), localPath, fileUpload.FileName);
@@ -74,17 +69,12 @@ namespace ShoppingList.Controllers
                 {
                     await fileUpload.CopyToAsync(stream);
                 }
-                
+
                 product.ProductImage = fileUpload.FileName;
             }
-
-            
-
-            // Ürünü veritabanına kaydetme
             _context.Products.Add(product);
             _context.SaveChanges();
             return RedirectToAction("Product", "Admin");
-
         }
         [HttpGet]
         public IActionResult UpdateProduct(int productId)
@@ -96,26 +86,20 @@ namespace ShoppingList.Controllers
             }).ToList();
 
             ViewBag.Categories = categories;
-
             var product = _context.Products.FirstOrDefault(p => p.ProductId == productId);
-
             if (product == null)
             {
-                return NotFound(); // Ürün bulunamazsa
+                return NotFound(); 
             }
-
             var productUpdateViewModel = new AdminAddFileViewModel
             {
                 ProductId = product.ProductId,
                 ProductName = product.ProductName,
                 CategoryId = product.CategoryId,
                 ProductImage = product.ProductImage
-                // Diğer özellikleri de ViewModel'e aktarabilirsiniz
             };
-
             return View(productUpdateViewModel);
         }
-
         [HttpPost]
         public IActionResult UpdateProduct(AdminAddFileViewModel viewModel)
         {
@@ -125,32 +109,23 @@ namespace ShoppingList.Controllers
                 return RedirectToAction("AddCategory", "Admin");
             }
             var product = _context.Products.FirstOrDefault(p => p.ProductId == viewModel.ProductId);
-
             if (product == null)
             {
-                return NotFound(); // Ürün bulunamazsa
+                return NotFound(); 
             }
-
-            // ViewModel'den alınan verilerle ürünü güncelle
             product.ProductName = viewModel.ProductName;
             product.CategoryId = viewModel.CategoryId;
             product.ProductImage = viewModel.ProductImage;
-            // Diğer özellikleri güncelleyebilirsiniz
-
-            _context.SaveChanges(); // Veritabanını güncelle
-                
-            return RedirectToAction("Product","Admin"); // Güncelleme tamamlandığında bir başka sayfaya yönlendir
-
+            _context.SaveChanges();
+            return RedirectToAction("Product", "Admin"); 
         }
-
         public IActionResult DeleteProduct(AdminAddFileViewModel adminEditFileViewModel)
         {
             var productsToDelete = _context.Products.Where(p => p.ProductId == adminEditFileViewModel.ProductId).SingleOrDefault();
 
-            if (productsToDelete==null)
+            if (productsToDelete == null)
             {
                 return NotFound();
-                // Handle the case where no products with the given CategoryId exist
             }
             else
             {
@@ -159,7 +134,7 @@ namespace ShoppingList.Controllers
                 return RedirectToAction("Product", "Admin");
             }
         }
-            public IActionResult Category()
+        public IActionResult Category()
         {
             var kategori = _context.Categories.ToList();
             return View(kategori);
@@ -172,36 +147,34 @@ namespace ShoppingList.Controllers
         [HttpPost]
         public IActionResult AddCategory(Category category)
         {
-            if(_context.Categories.Any(c => c.CategoryName == category.CategoryName))
+            if (_context.Categories.Any(c => c.CategoryName == category.CategoryName))
             {
-				TempData["ErrorMessage"] = "Kategori zaten var";
-				return RedirectToAction("AddCategory", "Admin");
-			}
-			_context.Categories.Add(category);
-                _context.SaveChanges();
-                return RedirectToAction("Category","Admin");
-               
+                TempData["ErrorMessage"] = "Kategori zaten var";
+                return RedirectToAction("AddCategory", "Admin");
+            }
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Category", "Admin");
         }
         [HttpGet]
         public IActionResult UpdateCategory(int categoryId)
         {
             var category = _context.Categories.FirstOrDefault(c => c.CategoryId == categoryId);
-			return View(category);
+            return View(category);
         }
         [HttpPost]
-		public IActionResult UpdateCategory(Category category)
-		{
-			if (_context.Categories.Any(c => c.CategoryName == category.CategoryName))
-			{
-				TempData["ErrorMessage"] = "Kategori zaten var";
-				return RedirectToAction("Category", "Admin");
-			}
-			_context.Categories.Update(category);
-			_context.SaveChanges();
+        public IActionResult UpdateCategory(Category category)
+        {
+            if (_context.Categories.Any(c => c.CategoryName == category.CategoryName))
+            {
+                TempData["ErrorMessage"] = "Kategori zaten var";
+                return RedirectToAction("Category", "Admin");
+            }
+            _context.Categories.Update(category);
+            _context.SaveChanges();
             TempData["SuccessMessage"] = "Kategori güncellendi";
-			return RedirectToAction("Category", "Admin");
-		}
-
+            return RedirectToAction("Category", "Admin");
+        }
         public IActionResult DeleteCategory(int categoryId)
         {
             var category = _context.Categories.Find(categoryId); ;
@@ -209,7 +182,5 @@ namespace ShoppingList.Controllers
             _context.SaveChanges();
             return RedirectToAction("Category", "Admin");
         }
-        
-
     }
 }
