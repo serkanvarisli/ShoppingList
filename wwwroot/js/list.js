9dcb80fbdc7d8fd2bc4c243fbb3c9742f081286b
Serkan Varışlı <87394796+serkanvarisli@users.noreply.github.com>
2023-09-01 19:26:33 +0300

    ajax

--- wwwroot/js/list.js
+++ wwwroot/js/list.js
@@ -5,22 +5,19 @@ $(document).ready(function () {
             selectedItems.push($(this).val());
         });
 
-        
             $.ajax({
                 url: '/Home/DeleteSelectedItems', // Silme iþlemi yapýlacak action'ýn yolunu belirtin
                 type: 'POST',
                 data: { selectedItems: selectedItems },
                 success: function (result) {
                     // Silme iþlemi baþarýlýysa sayfayý yenileyebilirsiniz veya baþka bir iþlem yapabilirsiniz.
+                    location.reload();
                 },
                 error: function () {
                     alert('Silme islemi sirasýnda bir hata oluþtu.');
-                    location.reload();
-
                 }
             });
-                    
-
+        window.location.href = "/Home/List?listId=" + productToDelete.ListId;
     });
 
     $('#startShopping').click(function () {
